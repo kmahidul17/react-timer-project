58d42c4e836565f6eb49974f18f4fc4fcdaa6edb
kmahidul17 <kmahidul17@gmail.com>
2020-10-16 21:30:08 +0600

    first app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Timer from './components/Timer';
+import ButtonController from './components/ButtonController';
+import ResetButton from './components/ResetButtons'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    timer: 0,
+    paused: true,
+
+  };
+
+
+  tick = () => {
+    this.setState({
+      timer: this.state.timer + 1
+    })
+  }
+
+
+  startTimer = () => {
+    this.interval = setInterval(this.tick, 1000);
+    this.setState({
+      paused: false
+    })
+  }
+
+  stopTimer = () => {
+    clearInterval(this.interval);
+    this.setState({ paused: true });
+  }
+  
+  onClickHandler = () => {
+
+    if(this.state.paused){
+      this.startTimer()
+    }
+    else{
+
+      this.stopTimer()
+    }
+    
+  }
+  
+onClickreset = () => {
+  this.setState({timer: 0 , paused: true})
+  clearInterval(this.interval)
+}
+
+
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Timer time={this.state.timer} />
+          <ButtonController paused={this.state.paused}  onhandler={this.onClickHandler} />
+          <ResetButton onClickreset={this.onClickreset}/>
+
+        </header>
+      </div>
+    );
+
+  }
+
+
 }
 
 export default App;
+
